57fc72033d8ac1de8fafa5049ac9ea7808fcfdd7
aptx113 <aptx113@gmail.com>
2020-04-22 17:32:59 +0800

    Setup ViewModelFactory with extension

--- app/src/main/java/com/danteyu/studio/weatherforecast/MainViewModel.kt
+++ app/src/main/java/com/danteyu/studio/weatherforecast/MainViewModel.kt
@@ -1,6 +1,13 @@
 package com.danteyu.studio.weatherforecast
 
+import androidx.lifecycle.ViewModel
+import com.danteyu.studio.weatherforecast.data.source.WeatherRepository
+
 /**
  * Created by George Yu on 2020/4/22.
+ *
+ * The [ViewModel] that is attached to the [MainActivity].
  */
-class MainViewModel
+class MainViewModel(private val weatherRepository: WeatherRepository) : ViewModel() {
+
+}
--- app/src/main/java/com/danteyu/studio/weatherforecast/factory/ViewModelFactory.kt
+++ app/src/main/java/com/danteyu/studio/weatherforecast/factory/ViewModelFactory.kt
@@ -1,5 +1,24 @@
 package com.danteyu.studio.weatherforecast.factory
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.danteyu.studio.weatherforecast.MainViewModel
+import com.danteyu.studio.weatherforecast.data.source.WeatherRepository
+import java.lang.IllegalArgumentException
+
 /**
  * Created by George Yu on 2020/4/22.
  */
+@Suppress("UNCHECKED_CAST")
+class ViewModelFactory constructor(
+    private val weatherRepository: WeatherRepository
+) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>) = with(modelClass) {
+        when {
+            isAssignableFrom(MainViewModel::class.java) -> MainViewModel(weatherRepository)
+
+            else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
+        }
+    } as T
+}
