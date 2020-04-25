2f3dc64d1546f68e893d8ce7b1348e042f044f7d
aptx113 <aptx113@gmail.com>
2020-04-25 13:39:04 +0800

    Build sealed class to hold two types of View in RecyclerView

--- app/src/main/java/com/danteyu/studio/weatherforecast/data/WeatherElement.kt
+++ app/src/main/java/com/danteyu/studio/weatherforecast/data/WeatherElement.kt
@@ -1,6 +1,8 @@
 package com.danteyu.studio.weatherforecast.data
 
 import android.os.Parcelable
+import com.danteyu.studio.weatherforecast.R
+import com.danteyu.studio.weatherforecast.util.Util.getDrawable
 import com.squareup.moshi.Json
 import kotlinx.android.parcel.Parcelize
 
@@ -18,10 +20,12 @@ data class WeatherElement(
         val items = mutableListOf<HomeItem>()
 
         temperatureList?.let {
-            for (temperature in it) {
+            it.forEach { temperature ->
                 items.add(HomeItem.WeatherForecastData(temperature))
+                items.add(HomeItem.Image(WeatherImage(getDrawable(R.drawable.icon_cwb))))
             }
         }
+
         return items
     }
 }
--- app/src/main/java/com/danteyu/studio/weatherforecast/data/WeatherImage.kt
+++ app/src/main/java/com/danteyu/studio/weatherforecast/data/WeatherImage.kt
@@ -1,6 +1,8 @@
 package com.danteyu.studio.weatherforecast.data
 
+import android.graphics.drawable.Drawable
+
 /**
  * Created by George Yu on 2020/4/24.
  */
-data class WeatherImage(val weatherImage: Int)
+data class WeatherImage(val weatherImage: Drawable?)
